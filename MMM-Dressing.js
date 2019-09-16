bac8a25605a1e69aa8311e14c66842558d0cd1a5
zuoyayong <zuoyayong@126.com>
2019-09-16 16:27:53 +0800

    add files

---
+++ MMM-Dressing.js
@@ -0,0 +1,143 @@
+const gui_state = {
+    init: 1,
+    start_record: 2,
+    finish_record: 3,
+    start_play: 4,
+    finish_play: 5
+};
+
+Module.register("MMM-Dressing", {
+  defaults: {
+    recordLength: 10, //unit second
+  },
+
+  started: false,
+  suspended: false,
+  state: gui_state.init,
+  mediaStreamTrack: null,
+  recorder: null,
+  filename: "",
+
+  resume: function() {
+    this.suspended = false;
+    this.started = true;
+    this.state = gui_state.init;
+    this.updateDom();
+  },
+  suspend: function () {
+    this.state = gui_state.init;
+    this.suspended = true;
+    if (this.mediaStreamTrack) this.mediaStreamTrack.stop();
+    //if (this.recorder) this.recorder.stop();
+    this.updateDom();
+  },
+
+  getStyles: function() {
+    return ["MMM-Dressing.css"];
+  },
+
+  start: function() {
+    console.log("Starting module: " + this.name);
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    switch (notification) {
+      case "10S_SAVE_DATA_RET":
+        this.filename = payload;
+        this.state = gui_state.finish_record;
+        this.updateDom();
+        break;
+      default:
+        break;
+    }
+  },
+
+  getDom: function() {
+    if (this.suspended == true || !this.started) {
+      return document.createElement("div");
+    }
+
+    switch (this.state) {
+      case gui_state.init: {
+        var self = this;
+        var wrapper = document.createElement("div");
+        wrapper.className = "wrapper";
+
+        var cell = document.createElement("div");
+        cell.className = "small cell";
+
+        var num = document.createElement("div");
+        num.className = "num";
+        num.innerText = "5 4 3 2 1 0";
+        cell.appendChild(num);
+        cell.addEventListener('animationend', function() {
+          self.state = gui_state.start_record;
+          self.updateDom();
+        });
+        wrapper.appendChild(cell);
+        return wrapper;
+      }
+      case gui_state.start_record: {
+        var wrapper = document.createElement("div");
+        var video = document.createElement("video");
+
+        let constraints = {
+            video: { width: 800, height: 500 },
+            audio: false
+        };
+        let promise = navigator.mediaDevices.getUserMedia(constraints);
+        promise.then((mediaStream) => {
+          this.mediaStreamTrack = typeof mediaStream.stop === 'function' ? mediaStream : mediaStream.getTracks()[0];
+          this.recorder = new MediaRecorder(mediaStream, {mimeType: 'video/webm'});
+          this.recorder.ondataavailable = event => {
+            let blob = new Blob([event.data], {
+              type: 'video/mp4'
+            });
+            let reader = new FileReader();
+            let _t = this;
+            reader.onload = function () {
+              _t.sendSocketNotification("10S_SAVE_DATA", reader.result);
+            };
+            reader.readAsArrayBuffer(blob);
+          };
+          this.recorder.start();
+          video.srcObject = mediaStream;
+          video.play();
+          setTimeout( () => {
+            this.recorder.stop(); 
+            this.mediaStreamTrack.stop();
+            this.state = gui_state.finish_record;
+            this.updateDom();
+          }, (this.config.recordLength + 1) * 1000);
+        });
+        wrapper.appendChild(video);
+
+        var blink_wrapper = document.createElement("div");
+        blink_wrapper.className = "blink_wrapper";
+        var blink = document.createElement("span");
+        blink.className = "blink";
+        blink.innerText = "Recording...";
+
+        blink_wrapper.appendChild(blink);
+        wrapper.appendChild(blink_wrapper);
+
+        return wrapper;
+      }
+      case gui_state.finish_record: {
+        var wrapper = document.createElement("div");
+        var video = document.createElement("video");
+        video.setAttribute("controls", "controls");
+        //video.setAttribute("loop", "loop");
+        video.src = "modules/MM-Modules/MMM-10Seconds/videos/" + this.filename;
+
+        wrapper.appendChild(video);
+        
+        //window.addEventListener("click", onclick);
+        return wrapper;
+      }
+      default: 
+        return document.createElement("div");
+    };
+
+  },
+});
---
+++ node_helper.js
@@ -0,0 +1,33 @@
+const NodeHelper = require('node_helper');
+var fs = require('fs');
+var moment = require('moment');
+
+module.exports = NodeHelper.create({
+  start: function() {
+    console.log("Starting node_helper for module: " + this.name);
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    switch (notification) {
+      case "10S_SAVE_DATA":
+        this.saveData(payload);
+        break;
+      default:
+        break;
+    }
+  },
+
+  saveData: function(payload) {
+    let buffer = new Buffer.from(payload);
+    var filename = moment().format("YYYYMMDDHHmmss") + ".mp4";
+    fs.writeFile("modules/MM-Modules/MMM-Dressing/videos/" + filename, buffer, {}, (err, res) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+    this.sendSocketNotification("10S_SAVE_DATA_RET", filename);
+  },
+
+
+});
